826c687bdc23e613b8c8ca6255e0e1457336a574
Md Vaseem <vaseemjabalijabali27@gmail.com>
2022-07-27 15:36:59 +0530

    component props

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,11 +1,23 @@
 export default {
-    template: `<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" :class="processing ? 'cursor-not-allowed' : '' ">
+    template: `<button :class="{
+            'text-white font-bold py-2 px-4 rounded' : true,
+            'bg-blue-500 hover:bg-blue-700 ' : type === 'primary',
+            'bg-red-500 hover:bg-red-700 ' : type === 'secondary',
+            'bg-green-500 hover:bg-green-700 ' : type === 'muted',
+            'cursor-not-allowed' : processing
+        }">
         <slot/>
         </button>`,
 
-        data() {
-            return {
-                processing: true
+        props:{
+            type:{
+                type: String,
+                default: 'primary'
+            },
+            processing: {
+                type: Boolean,
+                default: true
             }
-        }
+            
+        }, 
 }
